15c80acf7bf6a6eb8d4f39eb8e159909cc912ce3
Nataliia <koshmaknatalia@gmail.com>
2025-10-21 15:14:40 -0600

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.pastelpixelsstudio.com', 
+  generateRobotsTxt: true, // ✅ generates robots.txt
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 7000,
+};
